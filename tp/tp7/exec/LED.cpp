703b211f15a69c0a0d6d03162b305d74de109352
manel <manel.ben-jemaa@polymtl.ca>
2022-03-04 11:55:14 -0500

    ajout led.cpp

--- tp/tp7/exec/LED.cpp
+++ tp/tp7/exec/LED.cpp
@@ -1,6 +1,56 @@
-void eteindreLed() {
-PORTA = 0x00;  // eteindre la led 
+
+#include "LED.h"
+
+const uint8_t ROUGE = 0x02;
+const uint8_t VERT = 0x01;
+const uint8_t ETEINT = 0x00;
+const double ATTENTE_AMBRE=0.5;
+
+
+void choisirPort(Port port)
+{
+    switch (port)
+    {
+        case A:
+            
+            break;
+        case B:
+           
+            break;
+        case C:
+            
+            break;
+        case D:
+            
+            break;
+    }
+};
+
+void eteindreLed(uint8_t PORT) {
+PORT = ETEINT;  // eteindre la led 
 }
 
-void allumerLed() {
-PORTA = 0x02; // allumer la led rouge 
+
+void allumerLedRouge(uint8_t PORT) {
+    PORT = 0x02; // allumer la led rouge 
+}
+void allumerLedVERT(uint8_t PORT) {
+    PORT = 0x01; // allumer la led vert 
+}
+
+void ambre(uint8_t PORT) // allumer la led en ambré 
+{
+    PORT = ROUGE;     
+    _delay_ms(ATTENTE_AMBRE);
+    PORT =VERT; 
+    _delay_ms(ATTENTE_AMBRE);
+
+}
+
+
+
+
+
+
+
+
