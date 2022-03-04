6b7a1b70acc3a04827eb7bd1fdadfa00c6affa18
Anh Pham <anh-tuan.pham@polymtl.ca>
2022-03-04 11:12:16 -0500

    ajout methode initialisationTimerPWM

--- tp/tp7/lib/pwm.cpp
+++ tp/tp7/lib/pwm.cpp
@@ -20,3 +20,18 @@ unsigned nbPeriodes = unsigned(TEMPS_D_ACTIVITE_Second/b);
             _delay_ms(TEMPS_UNITAIRE);
     }      
 }
+
+// Initialisation du Timer0 pour generer du PWM
+void initialisationTimerPwm()
+{
+    cli();
+    DDRB |= (1 << PB2) | (1 << PB3);
+    TCNT0 = 0; // Set timer 0 to 0
+    // In SimulIde use the Fast PWM, but with the robot you must use the PWM Phase Correct 8-bit
+    TCCR0A =
+        (1 << WGM00) | // WGM03 WGM02 WGM01 WGM00 = 0 0 1 1 PWM, Phase correct , 10-bit
+        (1 << WGM01) | // Comment this line when you are in the robot to obtain the PWM Phase Correct 8-bit
+        (1 << COM0A1); // Clear OCnA/OCnB on Compare Match when upcounting. Set OCnA/OCnB on Compare Match when downcounting.
+    TCCR0B = (1 << CS01); // prescalor / 8
+    sei();
+}
--- tp/tp7/lib/pwm.h
+++ tp/tp7/lib/pwm.h
@@ -13,6 +13,7 @@ class pwm
 public:
 
    setPwm();
+   void initialisationTimerPwm();
    
 
 private:
