389393b4ff089fbb838c33d7956d0a1355647d63
manel <manel.ben-jemaa@polymtl.ca>
2022-03-10 15:01:01 -0500

    validation

--- tp/tp7/exec/main.cpp
+++ tp/tp7/exec/main.cpp
@@ -4,8 +4,10 @@
 #include "led.h"
 #include "rs232.h"
 #include "bouton.h"
+#include "moteur.h"
 int main() {
-/*#if defined(OFF)
+
+#if false 
     Led led (&PORTA,0,1);
     Bouton bouton (&PIND,2);
 
@@ -18,11 +20,32 @@ int main() {
     } 
 
 #endif
-*/
+
 // Tester RS232 
-//#if defined(ON)
+#if false  
 initialisationUART();
 char mots[] = "Le robot en INF1900\n";
 UARTTranmissionMot(mots);
-//#endif
+#endif
+
+//tester PMW 
+#if true
+    Moteur moteurs;
+    moteurs.initialisationTimer0Pwm();
+    int pourcentageOC0A = 100;
+    int pourcentageOC0B = 100;
+    while (true){
+        moteurs.avancerMoteur(pourcentageOC0A, pourcentageOC0B);
+        _delay_ms(2000);
+        moteurs.tournerDroiteMoteur(pourcentageOC0B);
+        DEBUG_PRINT((pourcentageOC0B));
+        _delay_ms(2000);
+        moteurs.tournerGaucheMoteur(pourcentageOC0A);
+        _delay_ms(2000);
+        moteurs.reculerMoteur(pourcentageOC0A, pourcentageOC0B);
+        _delay_ms(2000);
+        moteurs.arreterMoteur();
+        _delay_ms(2000);
+    }
+#endif 
 }
---
+++ tp/tp7/exec/main.d
@@ -0,0 +1,1 @@
+main.o: main.cpp ../lib/led.h ../lib/rs232.h ../lib/bouton.h
