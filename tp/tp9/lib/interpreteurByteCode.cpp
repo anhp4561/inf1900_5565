8ef42ccd25f6208c5418f21db5c99539219d39fa
Anh Pham <anh-tuan.pham@polymtl.ca>
2022-03-17 13:56:44 -0400

    debut de interpreteurByteCode

--- tp/tp9/lib/interpreteurByteCode.cpp
+++ tp/tp9/lib/interpreteurByteCode.cpp
@@ -1,1 +1,52 @@
 include "interpreteurByteCode.h"
+
+int main () {
+    // lire le byte code ici 
+
+    switch (byteCode){
+        case 0x01:
+            break;
+
+        case 0x02:
+            break;
+
+        case 0x44:
+            break;
+
+        case 0x54:
+            break;
+
+        case 0x48:
+            break;
+
+        case 0x09:
+            break;
+
+        case 0x60:
+            break;
+
+        case 0x61:
+            break;
+
+        case 0x62:
+            break;
+
+        case 0x63:
+            break;
+
+        case 0x64:
+            break;
+
+        case 0x65:
+            break;
+
+        case 0xC0:
+            break;
+
+        case 0xC1:
+            break;
+
+        case 0xFF:
+            break;
+    }
+}
