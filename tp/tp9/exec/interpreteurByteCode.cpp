9d8fffbd8fd062e5e77cd5c556df01166377beff
Oscar Arcand <oscar.arcand@polymtl.ca>
2022-03-17 16:35:27 -0400

    interpretation bytecode

--- tp/tp9/exec/interpreteurByteCode.cpp
+++ tp/tp9/exec/interpreteurByteCode.cpp
@@ -1,55 +1,75 @@
-// #include "interpreteurByteCode.h"
+#include "interpreteurByteCode.h"
 
-// int main () {
-//     while(true){
-//     // lire le byte code ici 
-//         if (byteCode == 0x01){ // 
-//             switch (byteCode){
-//                 case 0x01:
-//                     break;
+const int DEBUT = 0x01, 
+ATTENDRE = 0x02, 
+ALLUMERDEL = 0x44, 
+ETEINDREDEL = 0x45, 
+JOUERSON = 0x48, 
+STOPSON = 0x09, 
+STOPMOTEUR1 = 0x60, 
+STOPMOTEUR2 = 0x61, 
+AVANCER = 0x62, 
+RECULER = 0x63, 
+TOURNERDROITE = 0x64, 
+TOURNERGAUCHE = 0x65, 
+DEBUTBOUCLE = 0xC0, 
+FINBOUCLE = 0xC1, 
+FIN = 0xFF;
 
-//                 case 0x02:
-//                     break;
 
-//                 case 0x44:
-//                     break;
+bool enMarche = false;
 
-//                 case 0x54:
-//                     break;
+int main () {
+     while(true){
+     // lire le byte code ici 
+         if (enMarche || (byteCode == 0x01)) { // 
+             switch (byteCode){
+                 case DEBUT:
+                     enMarche = true
+                     break;
 
-//                 case 0x48:
-//                     break;
+                 case ATTENDRE:
+                     break;
 
-//                 case 0x09:
-//                     break;
+                 case ALLUMERDEL:
+                     break;
 
-//                 case 0x60:
-//                     break;
+                 case ETEINDREDEL:
+                     break;
 
-//                 case 0x61:
-//                     break;
+                 case JOUERSON:
+                     break;
 
-//                 case 0x62:
-//                     break;
+                 case STOPSON:
+                     break;
 
-//                 case 0x63:
-//                     break;
+                 case STOPMOTEUR1:
+                     break;
 
-//                 case 0x64:
-//                     break;
+                 case STOPMOTEUR2:
+                     break;
 
-//                 case 0x65:
-//                     break;
+                 case AVANCER:
+                     break;
 
-//                 case 0xC0:
-//                     break;
+                 case RECULER:
+                     break;
 
-//                 case 0xC1:
-//                     break;
+                 case TOURNERDROITE:
+                     break;
 
-//                 case 0xFF:
-//                     break;
-//             }
-//         }
-//     }
-// }
+                 case TOURNERGAUCHE:
+                     break;
+
+                 case DEBUTBOUCLE:
+                     break;
+
+                 case FINBOUCLE:
+                     break;
+
+                 case FIN:
+                     break;
+             }
+         }
+     }
+ }
