80922d5960e7151aae1234310f94e35dc01c0d7c
manel <manel.ben-jemaa@polymtl.ca>
2022-03-21 15:11:05 -0400

    ecriture memoire robot

--- tp/tp9/ecricremenoire/lireByteCode.cpp
+++ tp/tp9/ecricremenoire/lireByteCode.cpp
@@ -8,23 +8,39 @@
 
 
 #include <avr/io.h>
+#include <stdio.h>
 #include <util/delay.h>
 #include "rs232.h"
+#include "debug.h"
 #include <memoire_24.h>
-
-#define DERNIERE_ADRESSE_MEMOIRE 0x3E8 //1KB
+//#define DERNIERE_ADRESSE_MEMOIRE 0x10 //1KB
 
 int main()
-{
+   {
+	 DDRA=0XFF;
+
+ 	PORTA=0x01;
 	Memoire24CXXX notreMemoire = Memoire24CXXX();
+	
     initialisationUart();
+	
 	uint8_t donnee;
 	uint16_t adresseEcriture = 0x00;//initialisation pour écrire à la premère adresse mémoire
 	//DDRB = 0xff;//PORB en sortie
+	uint8_t tailleMoitie1 = receptionUart();
 
-	while (adresseEcriture < DERNIERE_ADRESSE_MEMOIRE) {
+    uint8_t tailleMoitie2 = receptionUart();
+     uint16_t tailleCode=tailleMoitie1 << 8;
+     tailleCode += tailleMoitie2;
+	
+	while (adresseEcriture < tailleCode-2) {
+	
 		donnee = receptionUart();//reception des données à écrire (un octet à la fois)
 		notreMemoire.ecriture(adresseEcriture, donnee);//écriture de la donnée reçu à l'adresse actuelle
+		_delay_ms(5);
 		adresseEcriture += 0x01;//adresse actuelle +1
+	
 	}
+	PORTA=0x02;
+		
 }
---
+++ tp/tp9/ecricremenoire/lireByteCode.d
@@ -0,0 +1,1 @@
+lireByteCode.o: lireByteCode.cpp rs232.h debug.h memoire_24.h
